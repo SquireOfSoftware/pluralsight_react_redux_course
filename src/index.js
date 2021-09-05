9596eb42ea00f4011d6b6a030ec5ea3602e4dee4
Joseph Tran <fred.smoth@gmail.com>
2021-09-05 20:55:51 +1000

    Starting app is done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import {render} from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css"
+import App from './components/App'
 
-function Hi() {
-    return <p>Hi</p>
-}
-
-render(<Hi />, document.getElementById("app"))
+render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("app"))
