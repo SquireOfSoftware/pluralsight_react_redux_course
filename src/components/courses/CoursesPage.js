877873877dff56897568e53aa0021c772ef1120c
Joseph Tran <fred.smoth@gmail.com>
2021-09-07 20:21:02 +1000

    Added a course list ui
    - also cleaned up the CoursesPage to use hooks

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,43 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
+import CourseList from './CourseList'
 
-class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: "",
-    },
-  };
+function CoursesPage(props) {
+  useEffect(() => {
+    props.actions.loadCourses().catch((error) => {
+      alert("Loading courses failed " + error);
+    });
+  }, []);
 
-  handleChange = (event) => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
-        {this.props.courses.map((course) => (
-          <div key={course.title}>{course.title}</div>
-        ))}
-      </form>
-    );
-  }
+  return (
+    <>
+      <h2>Courses Page</h2>
+      <CourseList courses={props.courses} />
+    </>
+  );
 }
 
 CoursesPage.propTypes = {
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,23 @@
-import * as types from './actionTypes'
+import * as types from "./actionTypes";
+import * as coursesApi from "../../api/courseApi";
 
 export function createCourse(course) {
-    return { type: types.CREATE_COURSE, course }
+  return { type: types.CREATE_COURSE, course };
+}
+
+export function loadCoursesSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function loadCourses() {
+  return function (dispatch) {
+    return coursesApi
+      .getCourses()
+      .then((courses) => {
+        dispatch(loadCoursesSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
 }
